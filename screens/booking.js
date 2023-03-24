7149d6e04736b671ae95403c4cae62e82a95cc9d
qsangdev <quangsang02018@gmail.com>
2023-03-25 01:07:10 +0700

    reupload

---
+++ screens/booking.js
@@ -0,0 +1,522 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Dimensions,
+  ScrollView,
+  Button,
+  TextInput,
+  Animated,
+  Modal,
+  Alert,
+  Image,
+  Keyboard,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Party} from '../assets';
+import DatePicker from 'react-native-date-picker';
+import NumericInput from 'react-native-numeric-input';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import moment from 'moment';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import 'react-native-get-random-values';
+import {v4 as uuidv4} from 'uuid';
+
+const {width} = Dimensions.get('window');
+const {height} = Dimensions.get('window');
+
+const Booking = ({route, navigation}) => {
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [tables, setTables] = useState(1);
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [checkTable, setCheckTable] = useState(false);
+  const [checkTime, setCheckTime] = useState(false);
+  const [remaining, setRemaining] = useState(0);
+
+  const today = new Date();
+
+  const item = route.params.item;
+
+  const [clicked, setClicked] = useState(1);
+
+  const handleClick = id => {
+    checkAvailableTable();
+    checkRealTime();
+    setClicked(id);
+  };
+
+  const onComfirmPress = () => {
+    if (name === '') {
+      return alert('Please enter your name');
+    } else if (number === '') {
+      return alert('Please enter your number..');
+    } else if (checkTable === false) {
+      return alert('The time you chose is full');
+    } else if (checkTime === false) {
+      return alert('The reservation time has passed');
+    } else if (number.match(/(84|0[3|5|7|8|9])+([0-9]{8})\b/g)) {
+      return setModalVisible(!modalVisible);
+    } else {
+      alert('Phone number is not in the correct format!')
+    }
+  };
+
+  const checkAvailableTable = () => {
+    const check = item.times.shift[clicked - 1].tables.filter(
+      e => e.status === 'unavailable',
+    ).length;
+    if (check < item.times.shift[clicked - 1].tables.length) {
+      return setCheckTable(true);
+    } else {
+      setCheckTable(false);
+    }
+  };
+
+  const calculatorMaxPeople = () => {
+    const check = item.times.shift[clicked - 1].tables.filter(
+      e => e.status === 'unavailable',
+    ).length;
+    const allTables = item.times.shift[clicked - 1].tables.length;
+    const remainingTables = (allTables - check) * 4;
+    if (remainingTables < allTables * 4) {
+      setRemaining(remainingTables);
+    } else console.log('full slot.');
+  };
+
+  const eatTime = moment(
+    moment(date).format('DD/MM') +
+      ' ' +
+      item.times.shift[clicked - 1].timeStart,
+    'DD/MM HH:mm',
+  );
+
+  const checkRealTime = () => {
+    if (eatTime.isBefore(moment())) {
+      setCheckTime(false);
+    } else {
+      setCheckTime(true);
+    }
+  };
+
+  useEffect(() => {
+    calculatorMaxPeople();
+  }, [clicked, remaining]);
+
+  useEffect(() => {
+    checkRealTime();
+  }, [clicked, checkTime, date]);
+
+  useEffect(() => {
+    checkAvailableTable();
+  }, [clicked, checkTable]);
+
+  const sendRequest = async () => {
+    try {
+      const data = JSON.parse(await AsyncStorage.getItem('tables')) || [];
+      const newOrder = {
+        id: uuidv4(),
+        name: name,
+        number: number,
+        date: moment(date).format('DD/MM/YYYY'),
+        time: item.times.shift[clicked - 1].timeStart,
+        restaurant: item.name,
+        location: item.location,
+        people: tables,
+      };
+      data.push(newOrder);
+      await AsyncStorage.setItem('tables', JSON.stringify(data));
+      alert('Booking Success');
+      Keyboard.dismiss();
+      navigation.navigate('Home');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <>
+      <ScrollView>
+        <Party width={width} height={width / 2} fill={'none'} />
+        <SafeAreaView>
+          <View style={styles.container}>
+            <View style={styles.description}>
+              <Text style={styles.descriptionText}>
+                Please enter your reservation information ..
+              </Text>
+            </View>
+            <View>
+              <TextInput
+                style={styles.input}
+                value={name}
+                placeholder="Name .."
+                onChangeText={e => setName(e)}
+              />
+              <TextInput
+                value={number}
+                style={styles.input}
+                placeholder="Phone number .."
+                keyboardType="numeric"
+                onChangeText={e => setNumber(e)}
+              />
+            </View>
+            <View style={styles.date}>
+              <Text style={styles.mealTimeText}>
+                Time: {moment(date).format('DD/MM/YYYY')}
+              </Text>
+              <Button
+                color="black"
+                title="Choose your date"
+                onPress={() => setOpen(true)}
+              />
+              <DatePicker
+                modal
+                mode="date"
+                open={open}
+                date={date}
+                minimumDate={new Date(today)}
+                maximumDate={new Date(today.setDate(today.getDate() + 3))}
+                onConfirm={date => {
+                  setOpen(false);
+                  setDate(date);
+                }}
+                onCancel={() => {
+                  setOpen(false);
+                }}
+              />
+            </View>
+            <View style={styles.people}>
+              <View>
+                <Text style={styles.mealTimeText}>How many people?:</Text>
+                <Text> *1 table with maximum slot for 4 adults</Text>
+              </View>
+              <NumericInput
+                type="plus-minus"
+                value={tables}
+                minValue={1}
+                maxValue={remaining}
+                totalWidth={100}
+                totalHeight={47}
+                onChange={value => setTables(value)}
+              />
+            </View>
+
+            <Text style={styles.mealTimeText}>Meal times :</Text>
+            <View style={styles.mealTimes}>
+              {item.times.shift.map(e => {
+                return (
+                  <TouchableOpacity
+                    key={e.id}
+                    onPress={() => handleClick(e.id)}
+                    style={[
+                      e.id === clicked
+                        ? styles.buttonActive
+                        : styles.buttonTime,
+                    ]}>
+                    <Text style={styles.buttonText}>
+                      <Ionicons
+                        color="white"
+                        size={15}
+                        name="time-outline"></Ionicons>{' '}
+                      {e.timeStart} - {e.timeEnd}
+                    </Text>
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+            <View style={styles.tableContainer}>
+              {item.times.shift.map(e => {
+                return (
+                  e.id === clicked && (
+                    <View style={styles.tables} key={e.id}>
+                      <View style={styles.tableAvaiText}>
+                        <Ionicons
+                          style={styles.note}
+                          size={30}
+                          name="restaurant-outline"></Ionicons>
+                        <Text style={styles.note}>
+                          Reserved:{' '}
+                          {
+                            e.tables.filter(e => e.status === 'unavailable')
+                              .length
+                          }{' '}
+                          / {e.tables.length}
+                        </Text>
+                      </View>
+                      {/* {e.tables.map(e => {
+                        return (
+                          <TouchableOpacity style={styles.table} key={e.id}>
+                            {e.status === 'available' ? (
+                              <CheckBox
+                                disabled={true}
+                                value={true}
+                                style={styles.checkbox}
+                              />
+                            ) : (
+                              <CheckBox
+                                disabled={true}
+                                value={false}
+                                style={styles.checkbox}
+                              />
+                            )}
+                            <Text style={styles.tableName}>{e.name}</Text>
+                          </TouchableOpacity>
+                        );
+                      })} */}
+                    </View>
+                  )
+                );
+              })}
+            </View>
+          </View>
+        </SafeAreaView>
+      </ScrollView>
+      <SafeAreaView>
+        <TouchableOpacity style={styles.buttonBooking} onPress={onComfirmPress}>
+          <Text style={styles.buttonDone}>Comfirm</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+      <Modal animationType="fade" transparent={true} visible={modalVisible}>
+        <View style={styles.modalView}>
+          <Image
+            style={styles.logo}
+            source={require('../assets/logo.png')}></Image>
+          <Text style={styles.mealTimeText}>Confirm information</Text>
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() => setModalVisible(!modalVisible)}>
+            <Ionicons
+              size={40}
+              color="red"
+              name="close-circle-outline"></Ionicons>
+          </TouchableOpacity>
+          <View style={styles.infoContainer}>
+            <View style={styles.info}>
+              <Ionicons
+                color="black"
+                size={30}
+                name="restaurant-outline"></Ionicons>
+              <Text style={styles.infoText}>{item.name}</Text>
+            </View>
+            <View style={styles.info}>
+              <Ionicons
+                color="black"
+                size={30}
+                name="location-outline"></Ionicons>
+              <Text style={styles.infoText}>{item.location}</Text>
+            </View>
+            <View style={styles.info}>
+              <Ionicons
+                color="black"
+                size={30}
+                name="calendar-outline"></Ionicons>
+              <Text style={styles.infoText}>
+                {moment(date).format('DD/MM/YYYY')}
+              </Text>
+            </View>
+            <View style={styles.info}>
+              <Ionicons color="black" size={30} name="time-outline"></Ionicons>
+              <Text style={styles.infoText}>
+                {item.times.shift[clicked - 1].timeStart} -{' '}
+                {item.times.shift[clicked - 1].timeEnd}
+              </Text>
+            </View>
+            <View style={styles.info}>
+              <Ionicons
+                color="black"
+                size={30}
+                name="people-outline"></Ionicons>
+              <Text style={styles.infoText}>{tables}</Text>
+            </View>
+            <View style={styles.info}>
+              <Ionicons
+                color="black"
+                size={30}
+                name="person-circle-outline"></Ionicons>
+              <Text style={styles.infoText}>{name}</Text>
+            </View>
+            <View style={styles.info}>
+              <Ionicons color="black" size={30} name="call-outline"></Ionicons>
+              <Text style={styles.infoText}>{number}</Text>
+            </View>
+          </View>
+          <TouchableOpacity onPress={sendRequest} style={styles.button}>
+            <Text style={styles.textStyle}>Start Booking</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+    </>
+  );
+};
+
+export default Booking;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    marginBottom: 30,
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    marginTop: 10,
+    borderColor: 'black',
+  },
+  description: {
+    width: '100%',
+  },
+  descriptionText: {
+    fontSize: 23,
+    fontWeight: '700',
+    color: 'black',
+  },
+  date: {
+    marginVertical: 15,
+  },
+  mealTimeText: {
+    fontSize: 20,
+    fontWeight: '700',
+    marginBottom: 5,
+    color: 'black',
+  },
+  mealTimes: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginVertical: 10,
+  },
+  buttonTime: {
+    padding: 8,
+    backgroundColor: 'silver',
+    borderRadius: 5,
+    backgroundColor: 'gray',
+  },
+  buttonText: {
+    fontWeight: '800',
+    color: 'white',
+  },
+  buttonActive: {
+    padding: 8,
+    backgroundColor: 'gray',
+    alignSelf: 'center',
+    borderRadius: 5,
+    backgroundColor: 'black',
+  },
+  people: {
+    justifyContent: 'space-between',
+    marginVertical: 10,
+    flexDirection: 'row',
+  },
+  note: {
+    color: 'black',
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+  tables: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    marginVertical: 10,
+  },
+  tableAvaiText: {
+    borderWidth: 1,
+    borderRadius: 5,
+    marginVertical: 5,
+    marginLeft: 5,
+    padding: 5,
+    borderColor: 'black',
+  },
+  table: {
+    padding: 5,
+    margin: 5,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  tableName: {
+    alignSelf: 'center',
+    fontWeight: '800',
+  },
+  buttonContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 10,
+  },
+  buttonBooking: {
+    width: '90%',
+    padding: 10,
+    borderRadius: 10,
+    marginHorizontal: 20,
+    marginBottom: 5,
+    backgroundColor: 'black',
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+  buttonDone: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+  modalView: {
+    zIndex: 1,
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.0,
+    elevation: 24,
+    height: height - 80,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 20,
+    elevation: 2,
+    backgroundColor: '#228B22',
+  },
+  textStyle: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  infoContainer: {
+    marginHorizontal: 20,
+  },
+  info: {
+    flexDirection: 'row',
+    marginHorizontal: 5,
+    marginVertical: 5,
+  },
+  infoText: {
+    fontSize: 15,
+    color: 'black',
+    fontWeight: '700',
+    textAlignVertical: 'center',
+    marginLeft: 10,
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+  },
+  logo: {
+    width: 60,
+    height: 55,
+    borderRadius: 5,
+    marginRight: 10,
+    zIndex: 1,
+  },
+});
---
+++ screens/restaurant.js
@@ -0,0 +1,292 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  ImageBackground,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {getDistance} from 'geolib';
+
+const {height} = Dimensions.get('window');
+const {width} = Dimensions.get('window');
+
+const IMG_WIDTH = width / 2 - 30;
+
+const Restaurant = ({route, navigation}) => {
+  const item = route.params.item;
+  const location = route.params.location;
+  const [imgActive, setImgActive] = useState(0);
+
+  onchange = nativeEvent => {
+    if (nativeEvent) {
+      const slide = Math.ceil(
+        nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width,
+      );
+      if (slide != imgActive) {
+        setImgActive(slide);
+      }
+    }
+  };
+
+  return (
+    <>
+      <TouchableOpacity
+        style={styles.buttonBack}
+        onPress={() => navigation.navigate('Home')}>
+        <Ionicons name="arrow-back" size={25} />
+      </TouchableOpacity>
+      <ScrollView>
+        <View>
+          <View style={styles.wrap}>
+            <View style={styles.wrapDot}>
+              {item.images.map((e, index) => (
+                <Text
+                  key={index}
+                  style={imgActive === index ? styles.dotActive : styles.dot}>
+                  ●
+                </Text>
+              ))}
+            </View>
+            <ScrollView
+              onScroll={({nativeEvent}) => onchange(nativeEvent)}
+              showsHorizontalScrollIndicator={false}
+              pagingEnabled
+              horizontal
+              style={styles.wrap}>
+              {item.images.map((e, index) => (
+                <ImageBackground
+                  key={index}
+                  resizeMode="stretch"
+                  source={e}
+                  style={styles.wrap}></ImageBackground>
+              ))}
+            </ScrollView>
+          </View>
+          <View style={styles.container}>
+            <View style={styles.titleContainer}>
+              <View style={styles.title}>
+                <Text style={styles.name}>{item.name}</Text>
+              </View>
+              <TouchableOpacity
+                style={styles.rating}
+                onPress={() => navigation.navigate('Rating')}>
+                <Ionicons name="star-outline" color="black" size={17} />
+                <Text style={styles.ratingText}>{item.rating}</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.location}>
+              <Text>{item.location}</Text>
+              <Text>
+                {getDistance(
+                  {
+                    latitude: location.latitude,
+                    longitude: location.longitude,
+                  },
+                  {
+                    latitude: item.latitude,
+                    longitude: item.longitude,
+                  },
+                ) / 1000}{' '}
+                KM
+              </Text>
+            </View>
+            <View style={styles.time}>
+              <Ionicons name="alarm-outline" color="black" size={22} />
+              <Text style={styles.timeText}>
+                <Text>Opening Time:</Text> {item.times.open_time} -{' '}
+                {item.times.close_time}
+              </Text>
+            </View>
+            <Text style={styles.resDescription}>{item.description}</Text>
+            <Text style={styles.menuText}>Menu</Text>
+            <View style={styles.menuContainer}>
+              {item.menu.map(e => {
+                return (
+                  <View style={styles.menu} key={e.id}>
+                    <Image style={styles.menuImage} source={{uri: e.image}} />
+                    <Text style={styles.menuItem}>{e.title}</Text>
+                    <Text>${e.price}</Text>
+                  </View>
+                );
+              })}
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+
+      <SafeAreaView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.buttonBooking}
+            onPress={() => navigation.navigate('Booking', {item: item})}>
+            <Text style={styles.buttonText}>Choose this Restaurant</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default Restaurant;
+
+const styles = StyleSheet.create({
+  wrap: {
+    width: width,
+    height: height / 2.5,
+  },
+  wrapDot: {
+    position: 'absolute',
+    top: 0,
+    flexDirection: 'row',
+    alignSelf: 'center',
+    zIndex: 1,
+  },
+  dotActive: {
+    margin: 3,
+    color: 'gray',
+    fontSize: 20,
+  },
+  dot: {
+    margin: 3,
+    color: 'white',
+    fontSize: 20,
+  },
+  buttonBack: {
+    height: 45,
+    width: 45,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 25,
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    zIndex: 1,
+  },
+  buttonShare: {
+    height: 45,
+    width: 45,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 25,
+    position: 'absolute',
+    top: 20,
+    right: 20,
+  },
+  container: {
+    padding: 20,
+    paddingTop: 30,
+    marginTop: -30,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    backgroundColor: 'white',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    marginBottom: 5,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  name: {
+    fontSize: 30,
+    color: 'black',
+    fontWeight: '700',
+  },
+  title: {
+    width: '70%',
+  },
+  rating: {
+    padding: 5,
+    paddingHorizontal: 10,
+    backgroundColor: 'yellow',
+    flexDirection: 'row',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  ratingText: {
+    fontSize: 16,
+    fontWeight: '600',
+    marginLeft: 5,
+    color: 'black',
+  },
+  timeText: {
+    fontSize: 16,
+    fontWeight: '600',
+    marginLeft: 5,
+    color: 'black',
+  },
+  resDescription: {
+    paddingVertical: 10,
+  },
+  location: {
+    marginBottom: 10,
+  },
+  time: {
+    padding: 5,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  menuContainer: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginVertical: 20,
+  },
+  menu: {
+    width: IMG_WIDTH,
+    marginBottom: 20,
+  },
+  circle: {
+    width: 10,
+    height: 10,
+    backgroundColor: 'silver',
+    borderRadius: 10,
+  },
+  menuItem: {
+    fontSize: 17,
+    fontWeight: '600',
+    color: 'gray',
+  },
+  menuText: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: 'black',
+  },
+  buttonContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+  },
+  buttonBooking: {
+    width: '100%',
+    padding: 5,
+    backgroundColor: 'gray',
+    borderRadius: 10,
+    backgroundColor: 'black',
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+  menuImage: {
+    width: '100%',
+    height: IMG_WIDTH + 30,
+    borderRadius: 20,
+  },
+});
